145a23f5b80f970794b8e01526fcc452e24ae87a
emery <emeryzhang0524@gmail.com>
2024-07-29 16:42:15 +0800

    修改登录

--- lib/pages/account/account_controller.dart
+++ lib/pages/account/account_controller.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:HexagonWarrior/api/api.dart';
-import 'package:HexagonWarrior/api/requests/prepare_request.dart';
 import 'package:HexagonWarrior/api/requests/reg_request.dart';
 import 'package:HexagonWarrior/api/requests/reg_verify_request.dart';
 import 'package:HexagonWarrior/api/requests/sign_request.dart';
@@ -11,31 +10,35 @@ import 'package:HexagonWarrior/extensions/extensions.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../api/requests/prepare_request.dart';
+
 class AccountController extends GetxController {
 
   Future<HttpResult> register(String email) async{
-    var res = await Api().prepare(PrepareRequest(email: email));
-    if(res.success) {
-      res = await Api().reg(RegRequest(captcha: "", email: "", origin: ""));
+
+     var res = await Api().reg(RegRequest(captcha: "", email: "", origin: ""));
       if(res.success) {
         res = await Api().regVerify(RegVerifyRequest(email: "", origin: ""));
         if(res.success) {
 
         }
-      }
-    }
+     }
+
     return res;
   }
 
-  Future<HttpResult> login(String email, {String? captcha}) async{
-
+  Future<HttpResult> prepare(String email) async{
     var res = await Api().prepare(PrepareRequest(email: email));
-    if(res.success) {
+    return res;
+  }
+
+
+  Future<HttpResult> login(String email, {String? captcha}) async{
       var res = await Api().sign(SignRequest(captcha: captcha, email: email, origin: ""));
       if(res.success) {
         res = await Api().signVerify(SignVerifyRequest(email: email, origin: ""));
       }
-    }
+
     return res;
   }
 
