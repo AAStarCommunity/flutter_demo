2ce0d4e4de0a27a460abf696d3abccd9148a55a4
emery <emeryzhang0524@gmail.com>
2024-08-16 17:04:28 +0800

    配置合约

--- lib/pages/account/account_controller.dart
+++ lib/pages/account/account_controller.dart
@@ -1,23 +1,32 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:HexagonWarrior/api/api.dart';
 import 'package:HexagonWarrior/api/generic_response.dart';
+import 'package:HexagonWarrior/api/local_http_client.dart';
 import 'package:HexagonWarrior/api/requests/reg_request.dart';
 import 'package:HexagonWarrior/api/requests/sign_request.dart';
 import 'package:HexagonWarrior/api/requests/tx_sign_request.dart';
 import 'package:HexagonWarrior/api/requests/verify_request_body.dart';
 import 'package:HexagonWarrior/api/response/account_info_response.dart';
 import 'package:HexagonWarrior/api/response/reg_response.dart';
+import 'package:HexagonWarrior/config/aa_start_client.dart';
+import 'package:HexagonWarrior/config/tx_configs.dart';
 import 'package:HexagonWarrior/extensions/extensions.dart';
 import 'package:HexagonWarrior/pages/account/models/account_info.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../api/requests/prepare_request.dart';
 import 'package:webauthn/webauthn.dart';
+import '../../config/tx_network.dart';
 import '../../utils/validate_util.dart';
 import '../../utils/webauthn/uint8list_converter.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:userop/userop.dart';
+import 'package:http/http.dart' as http;
 
 const _ORIGIN_DOMAIN = "https://demoweb.aastar.io";
 const _network = "optimism-sepolia";
@@ -93,11 +102,67 @@ class AccountController extends GetxController with StateMixin<AccountInfo>{
     return res;
   }
 
-  Future<void> txSign() async{
-    final publicKey = await Api().txSign(TxSignRequest());
-    CredentialRequestOptions.fromJson({
-      "publicKey" : {}
-    });
+  Future<void> txSign(String aa) async{
+    final bundlerConfig = op_sepolia.bundler[0];
+    final paymasterConfig = op_sepolia.paymaster[0];
+    final rpc = op_sepolia.rpc;
+
+    final bundlerRPC = bundlerConfig.url;
+    final aa = "0xdd";
+
+    final client = Web3Client("", http.Client());
+
+    final contractAddress = EthereumAddress.fromHex(op_sepolia.contracts.usdt);
+    final credentials = EthPrivateKey.fromHex('YOUR_PRIVATE_KEY');
+
+    String abiCode = await rootBundle.loadString("assets/contracts/TetherToken.json");
+    final contract = DeployedContract(
+      ContractAbi.fromJson(abiCode, op_sepolia.name),
+      contractAddress,
+    );
+
+    final func = contract.function("_mint");
+    final callData = await client.call(contract: contract, function: func, params: [
+      aa,
+      EtherAmount.fromInt(EtherUnit.ether, 10)
+    ]);
+
+
+    // final provider = BundlerJsonRpcProvider(0)
+    final opts = IPresetBuilderOpts()
+      ..entryPoint = EthereumAddress.fromHex(entryPointAddress)
+      ..factoryAddress = EthereumAddress.fromHex(factoryAddress)
+      ..paymasterMiddleware = verifyingPaymaster(paymasterConfig.url, {})
+      ..overrideBundlerRpc = bundlerRPC;
+
+    final simpleAccount = await SimpleAccount.init(
+      credentials,
+      bundlerRPC,
+      opts: opts,
+    );
+    final sender = simpleAccount.getSender();
+
+    IUserOperationBuilder uop = await simpleAccount.execute(Call(to: EthereumAddress.fromHex(sender), value: BigInt.zero, data: Uint8List.fromList(callData.map<int>((e)=>int.parse('$e')).toList())));
+
+    final IClientOpts iClientOpts = IClientOpts()
+      ..overrideBundlerRpc = bundlerRPC;
+
+    final opClient = await Client.init(rpc, opts: iClientOpts);
+
+    final sendOpts = ISendUserOperationOpts()
+      ..dryRun = false
+      ..onBuild = (IUserOperation ctx) {
+        print("Signed UserOperation: ${ctx.sender}");
+      };
+    final res = await opClient.sendUserOperation(uop, opts: sendOpts);
+    final ev = await res.wait();
+    final transactionHash = ev?.transactionHash;
+
+
+    // final publicKey = await Api().txSign(TxSignRequest());
+    // CredentialRequestOptions.fromJson({
+    //   "publicKey" : {}
+    // });
   }
 
   @override
