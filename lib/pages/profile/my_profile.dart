bcea266fb4b579c7f0cdc884a578352e822887d6
emery <emeryzhang0524@gmail.com>
2024-09-07 14:52:54 +0800

    修改Ui

--- lib/pages/profile/my_profile.dart
+++ lib/pages/profile/my_profile.dart
@@ -1,9 +1,10 @@
 import 'package:HexagonWarrior/pages/account/account_controller.dart';
 import 'package:HexagonWarrior/pages/settings/settings_page.dart';
 import 'package:HexagonWarrior/theme/app_colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:HexagonWarrior/extensions/extensions.dart';
 import '../../utils/ui/show_toast.dart';
 import '../account/login_page.dart';
 
@@ -19,18 +20,40 @@ class MyProfile extends GetView<AccountController> {
         return Column(children: [
           CircleAvatar(radius: 35, foregroundImage: AssetImage("assets/images/def_avatar.png")),
           const SizedBox(height: 24),
-          Text("${state?.email}").marginOnly(left: 12, bottom: 20)
-        ]).marginOnly(left: 24, top: 20);
+          Text("${state?.email}").marginOnly(left: 12, bottom: 20),
+          Stack(children: [
+            ClipRRect(child: AspectRatio(aspectRatio: 1.66,
+                child: Image.asset("assets/images/wallet_background.png",
+                width: context.width,
+                fit: BoxFit.cover, opacity: AlwaysStoppedAnimation(.2))),
+                borderRadius: BorderRadius.circular(12)),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(margin: EdgeInsets.only(left: 12, top: 12), child: Text("Balance", style: TextStyle(fontWeight: FontWeight.w600)),
+                  padding: EdgeInsets.symmetric(horizontal: 4),
+                  decoration: BoxDecoration(color: Color(0xFFED5151), borderRadius: BorderRadius.circular(4))),
+              Row(children: [
+                Expanded(child: Text("${state?.aa}", overflow: TextOverflow.ellipsis)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.copy, color: AppColors.caramelBrown))
+              ]).marginOnly(left: 12),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text.rich(TextSpan(children: [
+                  TextSpan(text: "\$  ", style: Theme.of(context).textTheme.titleSmall?.copyWith(fontWeight: FontWeight.w700, )),
+                  TextSpan(text: "${state?.usdtBalance?.trimTrailingZeros() ?? 0}", style: Theme.of(context).textTheme.titleLarge?.copyWith(fontWeight: FontWeight.w700, fontSize: 44))
+                ]))
+              ]),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                SizedBox(height: 30, child: CupertinoButton.filled(child: Text("charge".tr, style: TextStyle(fontSize: 14)), onPressed: () {
+                    showBiometricDialog(context, (index) {
+                      if(index == 1){
+                        controller.mintUsdt();
+                      }
+                    });
+                }, padding: EdgeInsets.symmetric(horizontal: 14), minSize: 20)),
+              ]).marginOnly(top: 12)
+            ]),
+          ]).marginSymmetric(horizontal: 24)
+        ]).marginOnly(top: 20);
       }),
-      Stack(children: [
-        ClipRRect(child: AspectRatio(aspectRatio: 2.34, child: Image.asset("assets/images/wallet_background.png", fit: BoxFit.cover, opacity: AlwaysStoppedAnimation(.2))), borderRadius: BorderRadius.circular(12)),
-        Container(margin: EdgeInsets.only(left: 16, top: 16), child: Text("Balance", style: TextStyle(fontWeight: FontWeight.w600)),
-            padding: EdgeInsets.symmetric(horizontal: 4),
-            decoration: BoxDecoration(color: Color(0xFFED5151), borderRadius: BorderRadius.circular(4))),
-        Container(margin: EdgeInsets.only(left: 16, top: 44), child: Column(children: [
-
-        ]))
-      ]).marginSymmetric(horizontal: 24),
       const SizedBox(height: 30),
       LayoutBuilder(builder: (_, __) {
         final list = [
@@ -55,6 +78,36 @@ class MyProfile extends GetView<AccountController> {
         },physics: NeverScrollableScrollPhysics(), shrinkWrap: true, ), clipBehavior: Clip.hardEdge);
       }),
     ]);
+  }
+
+  void showBiometricDialog(BuildContext context, ValueChanged<int> callback) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('dialogTitle'.tr),
+          content: Text('dialogContent'.tr),
+          actions: <Widget>[
+            TextButton(
+              child: Text('disagree'.tr),
+              onPressed: () {
+                Navigator.of(context).pop();
+                // 这里可以添加用户不同意时的逻辑
+                callback.call(0);
+              },
+            ),
+            TextButton(
+              child: Text('agree'.tr),
+              onPressed: () {
+                Navigator.of(context).pop();
+                callback?.call(1);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   _logout() async{
